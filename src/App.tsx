b7de185a1699096fc69f1a150b27c7b14b68d84b
Alfred Nelson <alfrednelson375@gmail.com>
2023-03-27 19:30:53 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="w-full h-[100svh] flex justify-center items-center">
+      Welcome to Relative.fi Trending Assets page
+    </div>
+  )
+}
+
+export default App
